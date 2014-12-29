d3c693873079c35ba381c9704907fed4e46538f3
Matt Filetto <matt.filetto@gmail.com>
2014-12-28 21:56:46 -0800

    Revert "klte-common: use TimeServices.apk"
    This reverts commit 3c93fee4906da4b4d63a5b8918dbe64144addd9e.

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -188,7 +188,6 @@ done
 PRODUCT_PACKAGES += \\
     libHevcSwDecoder \\
     libmm-abl \\
-    TimeService \\
     libtime_genoff
 
 \$(call inherit-product, vendor/$VENDOR/$DEVICE/$DEVICE-vendor-blobs.mk)
@@ -251,16 +250,6 @@ LOCAL_MODULE_PATH := \$(TARGET_OUT_VENDOR_SHARED_LIBRARIES)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_SUFFIX := .so
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-include \$(BUILD_PREBUILT)
-
-include \$(CLEAR_VARS)
-LOCAL_MODULE := TimeService
-LOCAL_MODULE_OWNER := $VENDOR
-LOCAL_SRC_FILES := proprietary/app/TimeService.apk
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_SUFFIX := \$(COMMON_ANDROID_PACKAGE_SUFFIX)
-LOCAL_MODULE_CLASS := APPS
-LOCAL_CERTIFICATE := platform
 include \$(BUILD_PREBUILT)
 
 include \$(CLEAR_VARS)
