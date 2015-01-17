d1c989f11155abe53084adbc2a4d4c9942f6471d
Matt Filetto <matt.filetto@gmail.com>
2015-01-16 23:39:09 -0800

    klte-common: Enable tethering without provisioning app
    Change-Id: I91a4ab216afe4d3968e4de427bfcd828d71e4411

--- system_prop.mk
+++ system_prop.mk
@@ -46,3 +46,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # WiFi
 PRODUCT_PROPERTY_OVERRIDES += \
     wifi.interface=wlan0
+
+# Tethering
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    net.tethering.noprovisioning=true
+
